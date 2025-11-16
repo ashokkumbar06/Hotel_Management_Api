aefa2a21195479b779be79101cd1839721024ef8
Ashok <ashokakumbar06@gmail.com>
2025-11-16 11:55:45 +0530

    Basic Serivce config

--- src/main/java/com/HMA/HotelManagementApiApplication.java
+++ src/main/java/com/HMA/HotelManagementApiApplication.java
@@ -8,11 +8,11 @@ public class HotelManagementApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelManagementApiApplication.class, args);
-        System.out.println("    A        SSSSS      H       H      OOOO      K      K  ");
+        System.out.println("    A       SSSSSSS     H       H     00OOOO     K      K  ");
         System.out.println("   A A      S     S     H       H     O    O     K    K    ");
-        System.out.println("  AAAAA     S     s     H H H H H     O    O     K  K      ");
+        System.out.println("  AA~AA     S     s     H H H H H     O    O     K K       ");
         System.out.println(" A     A    S     S     H       H     O    O     K    K    ");
-        System.out.println("A       A    SSSSS      H       H      OOOO      K      K  ");
+        System.out.println("A       A   SSSSSSS     H       H     00OOOO     K      K  ");
 	}
 
 }
